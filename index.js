2901dcf5369d5f307f429931685227f7516b530b
Zexal0807 <gallinar00@gmail.com>
2021-08-10 14:54:35 +0200

    add generate keys

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+const { generateKeyPair } = require('crypto');
 class IntranetManager {
 
 
+
+	async _generateKeyPair() {
+		return new Promise((resolve, reject) => {
+			generateKeyPair('rsa', {
+				modulusLength: 4096,
+				publicKeyEncoding: {
+					type: 'spki',
+					format: 'pem'
+				},
+				privateKeyEncoding: {
+					type: 'pkcs8',
+					format: 'pem',
+					cipher: 'aes-256-cbc',
+					passphrase: 'top secret'
+				}
+			}, (err, publicKey, privateKey) => {
+				if (err)
+					return rejects(err)
+				return resolve({
+					publicKey: publicKey,
+					privateKey: privateKey
+				});
+			});
+		});
+	}
 
 }
 
